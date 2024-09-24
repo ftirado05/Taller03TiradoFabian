aae25c3acdea279bfd5d4638f93e1578bfc00668
Carlos Correa <ccorrea14@cu.edu.co>
2024-09-24 12:41:36 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import milogo from '../src/assets/img/e5a978b8-6772-4c85-a50e-15581af7d483.png'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={milogo} alt="logo de Aguacate" height="300px" />
+        <h1>Hello Word</h1>
+        <p>Este es mi taller</p>
+      </div>
+    </>
+  )
+}
+
+export default App
